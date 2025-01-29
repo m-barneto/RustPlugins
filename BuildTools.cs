96a00c3a5db8450513aa8299bc0ace6255a93ff4
m-barneto <4347791+m-barneto@users.noreply.github.com>
2025-01-29 12:28:53 -0500

    Added skin and color settings to bgrade functionality.

--- BuildTools.cs
+++ BuildTools.cs
@@ -1,11 +1,14 @@
 ﻿#pragma warning disable IDE0051 // Remove unused private members
 
 using Facepunch;
+using Oxide.Core;
 using Oxide.Core.Libraries.Covalence;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using static Construction;
+using static Oxide.Plugins.BuildTools;
 
 namespace Oxide.Plugins {
     [Info("BuildTools", "Mattdokn", "1.0.0")]
@@ -14,9 +17,14 @@ namespace Oxide.Plugins {
         public const string USE_CREATIVE_CUPBOARD = "buildtools.creativecupboard";
         public const string USE_REMOVE_HAMMER = "buildtools.hammerremove";
 
+        public const string USE_BGRADE = "buildtools.bgrade";
+        public const string USE_SKINNED_GRADES = "buildtools.bgrade_skinned";
+
         public const float REPLENISH_CUPBOARD_INTERVAL = 60f;
 
         public List<BuildingPrivlidge> cupboards = new List<BuildingPrivlidge>();
+
+        public Dictionary<ulong, PlayerSelectionData> playerSelections = new Dictionary<ulong, PlayerSelectionData>();
 
         Coroutine replenish;
 
@@ -27,6 +35,14 @@ namespace Oxide.Plugins {
 
             permission.RegisterPermission(USE_CREATIVE_CUPBOARD, this);
             permission.RegisterPermission(USE_REMOVE_HAMMER, this);
+
+            permission.RegisterPermission(USE_BGRADE, this);
+            permission.RegisterPermission(USE_SKINNED_GRADES, this);
+
+            // Go through active players and setup player data
+            for (int i = 0; i < BasePlayer.activePlayerList.Count; i++) {
+                OnPlayerConnected(BasePlayer.activePlayerList[i]);
+            }
 
             // Go through all cupboards on server
             // Replace with tracked cupboards later
@@ -63,33 +79,62 @@ namespace Oxide.Plugins {
 
                     hitEnt.Kill();
                 }
+                if (input.WasJustPressed(BUTTON.FIRE_PRIMARY)) {
+                    // Have perm
+                    if (!permission.UserHasPermission(player.UserIDString, USE_BGRADE)) return;
+
+                    PlayerSelectionData data;
+                    if (!playerSelections.TryGetValue(player.userID, out data)) return;
+
+                    // Send out raycast and kill entity
+                    RaycastHit hit;
+                    if (!Physics.Raycast(player.eyes.HeadRay(), out hit, 25f)) return;
+
+                    BuildingBlock bb = hit.GetEntity() as BuildingBlock;
+
+                    if (bb == null) return;
+                    if (bb.OwnerID != player.userID) return;
+
+                    UpdateBuildingBlock(bb, data.GetGrade(), data.GetCurrentSkin().skin, data.GetCurrentColor());
+                }
             }
         }
 
         //OnServerCommand inventory.lighttoggle
+        // TODO can we switch to this void since we never want to return non-null
         object OnServerCommand(ConsoleSystem.Arg arg) {
-            if (arg.Connection == null || arg.cmd.FullName != "inventory.lighttoggle") {
-                return null;
-            }
+            if (arg.Connection == null) return null;
 
 
             var player = arg.Player();
             if (player == null) return null;
 
-            // If holding hammer
-            if (IsHoldingHammer(player)) {
-                if (player.serverInput.IsDown(BUTTON.DUCK) || player.serverInput.IsDown(BUTTON.SPRINT)) {
-                    // Go down a grade
-                    Puts("Down");
-                } else {
-                    //Go up a grade
-                    Puts("Up");
-                }
+            PlayerSelectionData playerSelectionData = null;
+            if (!playerSelections.TryGetValue(player.userID, out playerSelectionData)) {
+                return null;
             }
+
+
+            if (arg.cmd.FullName.Equals("inventory.lighttoggle")) {
+                // If holding hammer
+                if (IsHoldingHammer(player)) {
+                    if (player.serverInput.IsDown(BUTTON.DUCK) || player.serverInput.IsDown(BUTTON.SPRINT)) {
+                        // Go down a grade
+                        playerSelectionData.DecrementGrade();
+                    } else {
+                        //Go up a grade
+                        playerSelectionData.IncrementGrade();
+                    }
+                }
+            } else if (arg.cmd.FullName.Equals("global.setinfo") && arg.HasArg("client.selectedshippingcontainerblockcolour")) {
+                playerSelectionData.SetColor((uint)arg.GetInt(1, 1));
+            }
+
             return null;
         }
 
-        private void OnEntitySpawned(BuildingPrivlidge cupboard) {
+        // Detect when TC has been placed
+        void OnEntitySpawned(BuildingPrivlidge cupboard) {
             var player = BasePlayer.FindByID(cupboard.OwnerID);
             if (!permission.UserHasPermission(player.UserIDString, USE_CREATIVE_CUPBOARD)) return;
             // Authed user placed cupboard, add it to iterated list
@@ -100,6 +145,65 @@ namespace Oxide.Plugins {
                 cupboard.SendNetworkUpdateImmediate();
             });
 
+        }
+
+        // Setup player selection data
+        void OnPlayerConnected(BasePlayer player) {
+            if (playerSelections.ContainsKey(player.userID)) {
+                Puts($"Error! Player {player.UserIDString} : {player.displayName} already exists within playerSelections loaded data!");
+                return;
+            }
+
+            playerSelections.Add(player.userID, new PlayerSelectionData());
+        }
+        // Unload player selection data
+        void OnPlayerDisconnected(BasePlayer player, string reason) {
+            if (!playerSelections.ContainsKey(player.userID)) {
+                Puts($"Error! Player {player.UserIDString} : {player.displayName} does not exist within playerSelections loaded data!");
+                return;
+            }
+            playerSelections.Remove(player.userID);
+        }
+
+        // When player places structure handle auto bgrade and skin
+        private void OnEntityBuilt(Planner plan, GameObject gameObject) {
+            var player = plan?.GetOwnerPlayer();
+
+            if (player == null) return;
+            if (plan.isTypeDeployable) return;
+            if (!permission.UserHasPermission(player.UserIDString, USE_BGRADE)) return;
+
+            var buildingBlock = gameObject.GetComponent<BuildingBlock>();
+            if (buildingBlock == null) return;
+
+            if (!player.CanBuild()) return;
+
+            PlayerSelectionData playerData;
+            if (!playerSelections.TryGetValue(player.userID, out playerData)) {
+                return;
+            }
+
+            BuildingGrade.Enum grade = playerData.GetGrade();
+
+            if (Interface.Call("OnStructureUpgrade", buildingBlock, player, grade) != null) {
+                return;
+            }
+
+            UpdateBuildingBlock(buildingBlock, grade, playerData.GetCurrentSkin().skin, playerData.GetCurrentColor());
+        }
+
+        // Upgrade for free, select skin here
+        object OnPayForUpgrade(BasePlayer player, BuildingBlock block, ConstructionGrade gradeTarget) {
+            PlayerSelectionData playerData;
+            if (!playerSelections.TryGetValue(player.userID, out playerData)) return false;
+            Interface.Oxide.NextTick(() => {
+                playerData.SetSkin(block.grade, block.skinID);
+            });
+            return false;
+        }
+        // Place for free
+        object OnPayForPlacement(BasePlayer player, Planner planner, Construction construction) {
+            return false;
         }
         #endregion
 
@@ -118,12 +222,6 @@ namespace Oxide.Plugins {
             }
         }
 
-        /// <summary>
-        ///     Run a console command without any output into the console
-        /// </summary>
-        /// <param name="strCommand">Target command</param>
-        /// <param name="args">Target args</param>
-        /// <returns>A <see cref="ConsoleSystem.Arg" /> with the outcome of the command</returns>
         private static ConsoleSystem.Arg RunSilentCommand(string strCommand, params object[] args) {
             var command = ConsoleSystem.BuildCommand(strCommand, args);
             var arg = new ConsoleSystem.Arg(ConsoleSystem.Option.Unrestricted, command);
@@ -145,6 +243,113 @@ namespace Oxide.Plugins {
 
         }
 
+        void UpdateBuildingBlock(BuildingBlock bb, BuildingGrade.Enum grade, ulong skin = 0, uint color = 0) {
+            bb.ChangeGradeAndSkin(grade, skin, true);
+            bb.SetCustomColour(color);
+            bb.SetHealthToMax();
+            bb.StartBeingRotatable();
+            bb.SendNetworkUpdate();
+            bb.UpdateSkin();
+            bb.ResetUpkeepTime();
+            bb.GetBuilding()?.Dirty();
+        }
+
         #endregion
+
+        #region BGradeSkins
+        public class BuildingSkin {
+            public string name;
+            public ulong skin;
+            public BuildingSkin(string name, ulong skin) {
+                this.name = name;
+                this.skin = skin;
+            }
+        }
+        static Dictionary<BuildingGrade.Enum, List<BuildingSkin>> BGradeSkins = new Dictionary<BuildingGrade.Enum, List<BuildingSkin>>() {
+            {BuildingGrade.Enum.Twigs, new List<BuildingSkin>() {
+                new BuildingSkin("Twig", 0)
+            } },
+            {BuildingGrade.Enum.Wood, new List<BuildingSkin>() {
+                new BuildingSkin("Wood", 0),
+                new BuildingSkin("Frontier", 10232),
+                new BuildingSkin("Gingerbread", 2)
+            } },
+            {BuildingGrade.Enum.Stone, new List<BuildingSkin>() {
+                new BuildingSkin("Stone", 0),
+                new BuildingSkin("Adobe", 10220),
+                new BuildingSkin("Brick", 10223),
+                new BuildingSkin("Brutalist", 10225)
+            } },
+            {BuildingGrade.Enum.Metal, new List<BuildingSkin>() {
+                new BuildingSkin("Metal", 0),
+                new BuildingSkin("Container", 10221)
+            } },
+            {BuildingGrade.Enum.TopTier, new List<BuildingSkin>() {
+                new BuildingSkin("HQM", 0)
+            } },
+        };
+        #endregion
+
+        #region Player Selections
+        public class PlayerSelectionData {
+            BuildingGrade.Enum selectedGrade = BuildingGrade.Enum.Twigs;
+            Dictionary<BuildingGrade.Enum, BuildingSkin> selectedSkins;
+            uint selectedColor = 1;
+            public PlayerSelectionData() {
+                selectedSkins = new Dictionary<BuildingGrade.Enum, BuildingSkin>() {
+                    { BuildingGrade.Enum.Twigs, BGradeSkins[BuildingGrade.Enum.Twigs][0] },
+                    { BuildingGrade.Enum.Wood, BGradeSkins[BuildingGrade.Enum.Wood][0] },
+                    { BuildingGrade.Enum.Stone, BGradeSkins[BuildingGrade.Enum.Stone][0] },
+                    { BuildingGrade.Enum.Metal, BGradeSkins[BuildingGrade.Enum.Metal][0] },
+                    { BuildingGrade.Enum.TopTier, BGradeSkins[BuildingGrade.Enum.TopTier][0] },
+                };
+            }
+            public void SetGrade(BuildingGrade.Enum grade) {
+                selectedGrade = grade;
+            }
+            public void SetGrade(int grade) {
+                selectedGrade = (BuildingGrade.Enum)grade;
+            }
+            public void IncrementGrade() {
+                int g = (int)selectedGrade + 1;
+                if (g >= (int)BuildingGrade.Enum.Count) g = 0;
+                SetGrade(g);
+            }
+            public void DecrementGrade() {
+                int g = (int)selectedGrade - 1;
+                if (g <= 0) g = (int)BuildingGrade.Enum.TopTier;
+                SetGrade(g);
+            }
+            public BuildingGrade.Enum GetGrade() {
+                return selectedGrade;
+            }
+            public bool SetSkin(BuildingGrade.Enum grade, ulong skin) {
+                var buildingSkin = BGradeSkins[grade].Where((s) => s.skin == skin).First();
+                if (buildingSkin == null) {
+                    return false;
+                }
+                selectedSkins[grade] = buildingSkin;
+                return true;
+            }
+            public BuildingSkin GetSkin(BuildingGrade.Enum grade) {
+                return selectedSkins[grade];
+            }
+            public BuildingSkin GetCurrentSkin() {
+                return GetSkin(selectedGrade);
+            }
+            public bool SetColor(uint color) {
+                if (color <= 0 || color > 16) return false;
+                selectedColor = color;
+                return true;
+            }
+            public uint GetColor() {
+                return selectedColor;
+            }
+            public uint GetCurrentColor() {
+                if (!selectedSkins[selectedGrade].name.Equals("Container")) return 0;
+                return selectedColor;
+            }
+            #endregion
+        }
     }
 }
