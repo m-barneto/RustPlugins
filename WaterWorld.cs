bb1abc8a3c4b5e0dbea051aae8b2cd9604f2daab
mBarneto <4347791+m-barneto@users.noreply.github.com>
2025-08-05 21:22:50 -0400

    asd

---
+++ WaterWorld.cs
@@ -0,0 +1,13 @@
+﻿using Oxide.Plugins;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oxide.Plugins {
+    [Info("Island", "Mattdokn", "1.0.0")]
+    [Description("Island creation and management.")]
+    internal class WaterWorld {
+    }
+}
